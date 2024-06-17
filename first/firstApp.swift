5ba49211ed78f5be19004330a5a7f06b47f795f2
ayobami paul <aybamz181@gmail.com>
2024-06-17 22:32:09 +0100

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by MAC on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
